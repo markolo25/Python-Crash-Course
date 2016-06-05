b900b0c9d46e08ab5002c4b9847d1c953d67d155
Mark Mendoza <markolo25@gmail.com>
2016-06-05 08:02:31 -0700

    implemented settings, and started woring on ship

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -5,15 +5,35 @@ import pygame
 
 def run_game():
     pygame.init() #make game
-    screen = pygame.display.set_mode((1200,800)) #make window
+    ai_settings = Settings() #make settings object which has settings
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height)) #make window
     pygame.display.set_caption("Alien Invasion") #title for window
     
-    while True:
-        for event in pygame.event.get():
+    while True: #forever loop for the window
+        for event in pygame.event.get(): #wait for user to exit
             if event.type == pygame.QUIT:
                 sys.exit()
                 
-        pygame.display.flip()
+        screen.fill(ai_settings.bg_color)
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        pygame.display.flip() #redraw
     
     
     
